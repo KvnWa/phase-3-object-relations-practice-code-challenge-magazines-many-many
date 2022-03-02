466cb954264e019fe1afed3bdcbc5f4883aeae79
Kevin Wang <kevqwang@gmail.com>
2022-03-01 20:57:25 -0500

    commit tree e08fb246ab1e067a5dad1a8aa731f9122768cd34

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,13 @@
 class Magazine < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :reader, through: :subscriptions
+
+  def email_list
+    self.readers.map { |r| r.email }.join(";")
+  end
+
+  def self.most_popular
+    Magazine.all.max_by { |magazine| magazine.subscriptions.count }
+  end
   
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,18 @@
 class Reader < ActiveRecord::Base
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  def subscribe(magazine, price)
+    Subscription.create(magazine_id: magazine.id, price: price, reader_id: self.id)
+  end
+
+  def total_subscription_price
+    self.subscriptions.sum(:price)
+  end
+
+  def cancel_subscription(magazine)
+    self.subscriptions.where(magazine_id: magazine.id).destroy_all
+  end
+
   
 end
